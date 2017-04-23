cbb3e034989aa74357542ddffae62b964d0a0355
Kris Zima <kris.zima@thehaystackapp.com>
2017-04-23 22:05:29 +1000

    merge real-world into create-react-app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
+render(
+  <Root store={store} history={history} />,
+  document.getElementById('root')
+)
